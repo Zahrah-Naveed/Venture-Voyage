c7e98718d827060c3a9636b2fd2b4bbf9d4f5882
ayeshaMalik86 <ayesomestyles@gmail.com>
2024-05-27 22:00:58 +0500

    We created responsive navbar using reactjs and MUI. We simulated the login process of the user using global context and reducer to switch between logged and unlogged states of the user

---
+++ client/src/components/UserMenu.js
@@ -0,0 +1,33 @@
+import {useValue} from '../context/ContextProvider';
+import { Logout, Settings } from '@mui/icons-material'
+import {ListItemIcon, Menu, MenuItem} from '@mui/material'
+import React from 'react'
+const UserMenu = ({anchorUserMenu, setAnchorUserMenu}) => {
+    const{dispatch} = useValue()
+    const handleCloseUserMenu = ()=>{
+        setAnchorUserMenu(null)
+    }
+    return (
+        <Menu
+        anchorEl={anchorUserMenu}
+        open={Boolean(anchorUserMenu)}
+        onClose={handleCloseUserMenu}
+        onClick={handleCloseUserMenu}
+        >
+            <MenuItem>
+            <ListItemIcon>
+                <Settings fontsize="small" />
+            </ListItemIcon>
+            Profile
+            </MenuItem>
+            <MenuItem onClick={()=>dispatch({type:'UPDATE_USER', payload:null})}>
+            <ListItemIcon>
+                <Logout fontsize="small" />
+            </ListItemIcon>
+            Logout
+            </MenuItem>
+        </Menu>
+    )
+}
+
+export default UserMenu;
---
+++ client/src/components/user/UserIcons.js
@@ -0,0 +1,38 @@
+import React, {useState} from 'react';
+import { useValue } from '../../context/ContextProvider';
+import { Box, IconButton, Badge, Tooltip, Avatar } from '@mui/material';
+import { Mail, Notifications } from '@mui/icons-material';
+import UserMenu from '../UserMenu';
+
+
+const UserIcons = () => {
+    const 
+    {state:{currentUser},
+} = useValue();
+
+    const[anchorUserMenu, setAnchorUserMenu] = useState(null)
+        return (
+            <Box>
+                <IconButton size = 'large' color = 'inherit'>
+                    <Badge color ='error' badgeContent={5}>
+                        <Mail />
+                    </Badge>
+                </IconButton>
+                <IconButton size = 'large' color = 'inherit'>
+                    <Badge color ='error' badgeContent={20}>
+                        <Notifications/>
+                    </Badge>
+                </IconButton>
+                <Tooltip title = 'Open User Settings'>
+                    <IconButton onClick={(e)=>setAnchorUserMenu(e.currentTarget)}>
+                        <Avatar src = {currentUser?.photoURL} alt={currentUser?.name}>
+                            {currentUser?.name?.charAt(0).toUpperCase()}
+                        </Avatar>
+                    </IconButton>
+                </Tooltip>
+                <UserMenu {...{anchorUserMenu, setAnchorUserMenu}}/>
+            </Box>
+        )
+    }
+
+    export default UserIcons;
