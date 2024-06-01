fa76525e12724a956b3bd27071334044ac7b7a5f
Zahrah-Naveed <zahrahnaveed.1911@gmail.com>
2024-06-01 22:44:36 +0500

    added the price Slider Filter to filter the rooms depending on the price value

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   AppBar,
   Box,
@@ -9,11 +9,10 @@ import {
   Typography,
 } from '@mui/material';
 import { Lock, Menu } from '@mui/icons-material';
-import photoURL from '../profile.jpg';
+
 import { useValue } from '../context/ContextProvider';
 import UserIcons from './user/UserIcons';
-
-const user = { name: 'test', photoURL };
+import Sidebar from './sidebar/Sidebar';
 
 const NavBar = () => {
   const {
@@ -21,45 +20,55 @@ const NavBar = () => {
     dispatch,
   } = useValue();
 
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <AppBar>
-      <Container maxWidth="lg">
-        <Toolbar disableGutters>
-          <Box sx={{ mr: 1 }}>
-            <IconButton size="large" color="inherit">
-              <Menu />
-            </IconButton>
-          </Box>
-          <Typography
-            variant="h6"
-            component="h1"
-            noWrap
-            sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}
-          >
-            You Are Welcome
-          </Typography>
-          <Typography
-            variant="h6"
-            component="h1"
-            noWrap
-            sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
-          >
-            YRW
-          </Typography>
-          {!currentUser ? (
-            <Button
-              color="inherit"
-              startIcon={<Lock />}
-              onClick={() => dispatch({ type: 'OPEN_LOGIN' })}
+    <>
+      <AppBar>
+        <Container maxWidth="lg">
+          <Toolbar disableGutters>
+            <Box sx={{ mr: 1 }}>
+              <IconButton
+                size="large"
+                color="inherit"
+                onClick={() => setIsOpen(true)}
+              >
+                <Menu />
+              </IconButton>
+            </Box>
+            <Typography
+              variant="h6"
+              component="h1"
+              noWrap
+              sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}
             >
-              Login
-            </Button>
-          ) : (
-            <UserIcons />
-          )}
-        </Toolbar>
-      </Container>
-    </AppBar>
+              You Are Welcome
+            </Typography>
+            <Typography
+              variant="h6"
+              component="h1"
+              noWrap
+              sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
+            >
+              YRW
+            </Typography>
+            {!currentUser ? (
+              <Button
+                color="inherit"
+                startIcon={<Lock />}
+                onClick={() => dispatch({ type: 'OPEN_LOGIN' })}
+              >
+                Login
+              </Button>
+            ) : (
+              <UserIcons />
+            )}
+          </Toolbar>
+        </Container>
+      </AppBar>
+      <Toolbar />
+      <Sidebar {...{ isOpen, setIsOpen }} />
+    </>
   );
 };
 
